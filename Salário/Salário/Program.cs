b8372cbca5d17e14c5c8234a4e9703e7411120b2
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:26:25 -0300

    Add files via upload

---
+++ Salário/Salário/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Comissão por venda\r\n");
+
+double soma = 0;
+
+Console.Write("Escreva quantas vendas um funcionário fez em uma concessionária: R$ ");
+int vendas = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int i = 1;  i <= vendas; i++)
+{
+    Console.Write($"Escreva o valor da {i}° venda: R$ ");
+    float valor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+
+    double comissao = valor * 0.05;
+
+    soma += comissao;
+
+}
+
+Console.Write("Agora o atual salário deste funcionário: R$ ");
+float salario = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+double ganho = salario + soma;
+
+Console.Write($"Este funcionário terá um ganho de 5% das vendas, portanto receberá no próximo salário um total de R$ {ganho}");
