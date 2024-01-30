e5cd9753526a5df9253dc28639c19dc791bb149a
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:22:03 -0300

    Add files via upload

---
+++ Carro/Carro/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Carro\r\n");
+
+Console.Write("Escreva o preço de fábrica de um carro: R$");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+double d = n1 + (n1 * 28 / 100);
+double i = n1 + (n1 * 45 / 100);
+double r = n1 + d + i;
+
+Console.WriteLine($"O valor deste carro é de R${r.ToString("N2")}.");
