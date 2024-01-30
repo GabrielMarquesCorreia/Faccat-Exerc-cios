bd2eaf63576ff42fefb1e862ecc8265d0e13c44d
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:23:38 -0300

    Add files via upload

---
+++ maçãs/maçãs/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Maçãs\r\n");
+
+Console.Write("Escreva quantas maçãs você deseja comprar em uma feira: ");
+int macas = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+if (macas < 12)
+{
+    double r = macas * 1.30;
+    Console.WriteLine($"Você decidiu comprar {macas} maçãs, portanto gastará um total de R${r.ToString("N2")}");
+}
+else
+{
+    Console.WriteLine($"Você decidiu comprar {macas} maçãs, portanto gastará um total de R${macas}");
+}
