122e139fa2ccbce721a7d9a57e834c0a0a17b83f
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:25:08 -0300

    Add files via upload

---
+++ Positivo ou negativo/Positivo ou negativo/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Postivo ou negativo\r\n");
+
+Console.Write("Escreva um número: ");
+float num = float.Parse(Console.ReadLine());
+
+if (num < 0 )
+{
+    Console.WriteLine("Este número é negativo.");
+}
+else
+{
+    Console.WriteLine("Este número é positivo.");
+}
