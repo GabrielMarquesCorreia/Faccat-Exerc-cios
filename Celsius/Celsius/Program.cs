363625c555974e37eb577ac917aadb516490e01d
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:22:32 -0300

    Add files via upload

---
+++ Celsius/Celsius/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Celsius\r\n");
+
+Console.Write("Escreva um valor de graus em Fahrenheit: ");
+int fahrenheit = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+double celsius = (fahrenheit - 32) * 5 / 9;
+
+Console.WriteLine($"O valor de {fahrenheit}°F equivale ao mesmo que {celsius}°C.");
