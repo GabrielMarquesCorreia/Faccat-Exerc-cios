5f055c0fafe662687c15d70b904c3ef991d2b26b
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:24:10 -0300

    Add files via upload

---
+++ Maior, menor que 10/Maior, menor que 10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Maior, menor que 10\r\n");
+
+Console.Write("Escreva um número: ");
+float num = float.Parse(Console.ReadLine());
+
+if (num > 10)
+{
+    Console.WriteLine("Este número é maior que 10!!");
+}
+else if (num == 10)
+{
+    Console.WriteLine("Este número é igual a 10!!");
+}
+else
+{
+    Console.WriteLine("Este número é menor que 10!!");
+}
