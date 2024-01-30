a3e95f72778a64557da1047d9eac1f8bc796085b
GabrielMarquesCorreia <99968918+GabrielMarquesCorreia@users.noreply.github.com>
2024-01-30 15:21:28 -0300

    Add files via upload

---
+++ Antecessor/Antecessor/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Antecessor\r\n");
+
+Console.Write($"Escreva um número: ");
+int n = int.Parse( Console.ReadLine() );
+double r = n - 1;
+
+Console.WriteLine($"O antecessor do número {n} é: {r}");
